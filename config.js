5012b94579a31d3739177b9b5f70331c4ccd3a8a
藍月暗い <Kurai32761@gmail.com>
2025-02-01 18:40:16 +0700

    Update config.js

--- config.js
+++ config.js
@@ -10,18 +10,20 @@ module.exports = {
   SupportServer: "https://discord.gg/xQF9f9yUEM",
   embedTimeout: 5, 
   errorLog: "", 
-  nodes: [
+nodes: [
      {
-      "name": "Koi Node V4";
-      "password": "prplmoe.me";
-      "host": "lavav4.prplmoe.me";
-      "port": 1118;
-      "secure": false
+      name: "Koi Node V4",
+      password: "prplmoe.me",
+      host: "lavav4.prplmoe.me",
+      port: 1118,
+      secure: false
     },
     {
-      "name": "Muzykant v4",
-      "password": "https://discord.gg/v6sdrD9kPh",
-      "host": "lavalink_v4.muzykant.xyz",
-      "port": 80,
-      "secure": false
+      name: "ChalresNaig Node",
+      password: "NAIGLAVA-dash.techbyte.host",
+      host: "lavahatry4.techbyte.host",
+      port: 3000,
+      secure: false
     }
+  ]
+}
