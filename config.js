37b6b0ba57287001fc6be53eff236ed247045cdd
藍月暗い <Kurai32761@gmail.com>
2025-02-01 18:29:50 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,4 @@
 
-
 module.exports = {
   TOKEN: "",
   language: "en",
@@ -13,18 +12,16 @@ module.exports = {
   errorLog: "", 
   nodes: [
      {
-      name: "Koi Node V4",
-      password: "prplmoe.me",
-      host: "lavav4.prplmoe.me",
-      port: 1118,
+      name: "Koi Node V4";
+      password: "prplmoe.me";
+      host: "lavav4.prplmoe.me";
+      port: 1118;
       secure: false
     },
     {
-      name: "ChalresNaig Node",
-      password: "NAIGLAVA-dash.techbyte.host",
-      host: "lavahatry4.techbyte.host",
-      port: 3000,
+      name: "saher"
+      password: "saher.inzeworld.com";
+      host: "lava.inzeworld.com";
+      port: 3128;
       secure: false
-    }
-  ]
 }
